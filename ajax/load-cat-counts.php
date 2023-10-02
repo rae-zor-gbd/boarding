b1cce7eee0f2868e4ab9dae859459e716e6f23d2
carsoncalcutt <calcutt.carson@gmail.com>
2023-10-02 18:03:55 -0400

    Update navbar counts & stylesheet

--- ajax/load-cat-counts.php
+++ ajax/load-cat-counts.php
@@ -3,11 +3,11 @@ include '../assets/config.php';
 if (isset($_POST['startDate']) AND isset($_POST['endDate'])) {
   $startDate=$_POST['startDate'];
   $endDate=$_POST['endDate'];
-  $sql_condo_count="SELECT COUNT(condoID) AS availableCondos FROM condos WHERE condoID NOT IN (SELECT condoID FROM cats_reservations WHERE checkIn<='$endDate' AND checkOut>='$startDate')";
+  $sql_condo_count="SELECT COUNT(condoID) AS availableCondos FROM condos WHERE status!='Disabled' AND condoID NOT IN (SELECT condoID FROM cats_reservations WHERE checkIn<='$endDate' AND checkOut>='$startDate')";
   $result_condo_count=$conn->query($sql_condo_count);
   $row_condo_count=$result_condo_count->fetch_assoc();
   $condoCount=$row_condo_count['availableCondos'];
-  $sql_weekend_count="SELECT COUNT(catReservationID) AS weekendCheckOuts FROM cats_reservations WHERE checkIn<='$endDate' AND checkOut>='$startDate' AND WEEKDAY(checkOut) IN (4, 5, 6)";
+  $sql_weekend_count="SELECT COUNT(catReservationID) AS weekendCheckOuts FROM cats_reservations WHERE checkIn<='$endDate' AND checkOut>='$startDate' AND checkOut<='$endDate' AND WEEKDAY(checkOut) IN (4, 5, 6)";
   $result_weekend_count=$conn->query($sql_weekend_count);
   $row_weekend_count=$result_weekend_count->fetch_assoc();
   $weekendCount=$row_weekend_count['weekendCheckOuts'];
--- ajax/load-dog-counts.php
+++ ajax/load-dog-counts.php
@@ -3,11 +3,11 @@ include '../assets/config.php';
 if (isset($_POST['startDate']) AND isset($_POST['endDate'])) {
   $startDate=$_POST['startDate'];
   $endDate=$_POST['endDate'];
-  $sql_room_count="SELECT COUNT(roomID) AS availableRooms FROM rooms WHERE roomID NOT IN (SELECT roomID FROM dogs_reservations WHERE checkIn<='$endDate' AND checkOut>='$startDate')";
+  $sql_room_count="SELECT COUNT(roomID) AS availableRooms FROM rooms WHERE status!='Disabled' AND roomID NOT IN (SELECT roomID FROM dogs_reservations WHERE checkIn<='$endDate' AND checkOut>='$startDate')";
   $result_room_count=$conn->query($sql_room_count);
   $row_room_count=$result_room_count->fetch_assoc();
   $roomCount=$row_room_count['availableRooms'];
-  $sql_weekend_count="SELECT COUNT(dogReservationID) AS weekendCheckOuts FROM dogs_reservations WHERE checkIn<='$endDate' AND checkOut>='$startDate' AND WEEKDAY(checkOut) IN (4, 5, 6)";
+  $sql_weekend_count="SELECT COUNT(dogReservationID) AS weekendCheckOuts FROM dogs_reservations WHERE checkIn<='$endDate' AND checkOut>='$startDate' AND checkOut<='$endDate' AND WEEKDAY(checkOut) IN (4, 5, 6)";
   $result_weekend_count=$conn->query($sql_weekend_count);
   $row_weekend_count=$result_weekend_count->fetch_assoc();
   $weekendCount=$row_weekend_count['weekendCheckOuts'];
