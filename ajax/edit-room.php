5f80c55a63acaeac69a08b9fdd6acb19e1716959
Carson Calcutt <46988440+carsoncalcutt@users.noreply.github.com>
2024-09-12 13:46:23 -0400

    Remove last initial abbreviation logic

--- ajax/edit-room.php
+++ ajax/edit-room.php
@@ -5,11 +5,6 @@ if (isset($_POST['id']) AND isset($_POST['room']) AND isset($_POST['lastName']) 
   $roomID=mysqli_real_escape_string($conn, $_POST['room']);
   $lastName=mysqli_real_escape_string($conn, trim($_POST['lastName']));
   $dogName=mysqli_real_escape_string($conn, trim($_POST['dogName']));
-  $secondToLastCharacter=substr($dogName, -2, 1);
-  $lastCharacter=substr($dogName, -1, 1);
-  if (isset($lastCharacter) AND $lastCharacter!='' AND $secondToLastCharacter==' ') {
-    $dogName.=".";
-  }
   $checkIn=$_POST['checkIn'];
   $checkOut=$_POST['checkOut'];
   $sql_update="UPDATE dogs_reservations SET roomID='$roomID', lastName='$lastName', dogName='$dogName', checkIn='$checkIn', checkOut='$checkOut' WHERE dogReservationID='$id'";
